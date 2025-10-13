d0c70be8e2be15a600590d826bd32b76e0a58db5
Eslam-Sayed7 <eslam.sayed.ed@gmail.com>
2025-10-13 22:21:47 +0300

    edit typo

--- soft-util/src/main/java/com/example/softcomputing/genetic/operators/mutation/MutationStrategy.java
+++ soft-util/src/main/java/com/example/softcomputing/genetic/operators/mutation/MutationStrategy.java
@@ -2,5 +2,5 @@ package com.example.softcomputing.genetic.operators.mutation;
 import com.example.softcomputing.genetic.chromosome.Chromosome;
 
 public interface MutationStrategy<G , C extends Chromosome<G> > {
-	Chromosome<G> mutate(Chromosome<G> individual , Double mutationRate);
+	C mutate(C individual , Double mutationRate);
 }
